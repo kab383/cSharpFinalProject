8af1d2ee9a70f5160c2ccb7a275d5f4d075ddd8d
Kristian Beall <beallkristian@gmail.com>
2022-05-28 09:35:54 -0400

    built claim constructor in claim.cs

--- KomodoClaims/KomodoClaims.ConsoleApp/Program.cs
+++ KomodoClaims/KomodoClaims.ConsoleApp/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using KomodoClaims.ConsoleApp;
+UserInterface ui = new UserInterface();
+
+ui.Run();
--- KomodoClaims/KomodoClaims.Repository/Claim.cs
+++ KomodoClaims/KomodoClaims.Repository/Claim.cs
@@ -7,6 +7,23 @@ namespace KomodoClaims.Repository
 {
     public class Claim
     {
-        
+        public int ClaimID { get; set; }
+        public string ClaimType { get; set; }
+        public string Description { get; set; }
+        public decimal ClaimAmount { get; set; }
+        public DateTime DateOfIncident { get; set; }
+        public DateTime DateOfClaim { get; set; }
+        public bool IsValid { get; set; }
+
+        public Claim(int claimID, string claimType, string description, decimal claimAmount, DateTime dateOfIncident, DateTime dateOfClaim, bool isValid)
+        {
+            ClaimID = claimID;
+            ClaimType = claimType;
+            Description = description;
+            ClaimAmount = claimAmount;
+            DateOfIncident = dateOfIncident;
+            DateOfClaim = dateOfClaim;
+            IsValid = isValid;
+        }
     }
 }

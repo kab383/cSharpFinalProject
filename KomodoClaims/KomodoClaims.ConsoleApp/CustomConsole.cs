b87222a25bff2eb2fe179651b4f1bf2f24537a45
Kristian Beall <beallkristian@gmail.com>
2022-05-28 21:32:08 -0400

    working through bugs with options 2 and 3 in switch case. Code is laid out, but currently only option 1 is operable

--- KomodoClaims/KomodoClaims.ConsoleApp/CustomConsole.cs
+++ KomodoClaims/KomodoClaims.ConsoleApp/CustomConsole.cs
@@ -2,11 +2,75 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using KomodoClaims.Repository;
 
 namespace KomodoClaims.ConsoleApp
 {
     public class CustomConsole
     {
+        ClaimsRepository _claimsRepo = new ClaimsRepository();
+
+        public void PrintMainMenu()
+        {
+            Console.WriteLine("1. See all claims\n" +
+                                    "2. Take care of next claim\n" +
+                                    "3. Enter a new claim");
+        }
+
+        public void PrintClaimsHeadings()
+        {
+            Console.WriteLine("ClaimID" + " " + "Type" + " " + "Description" + " " + "Amount" + " " + "DateOfAccident" + " " + "DateOfClaim" + " " + "IsValid");
+        }
+
+        public string AgentInput()
+        {
+            return Console.ReadLine();
+        }
+
+        // public int AgentInputInt()
+        // {
+        //     return int.Parse(Console.ReadLine());
+ 
+        // }
+
+        // public ClaimType AgentInputClaimType()
+        // {
+        //     return 
+        // }
+
+        // public decimal AgentInputDecimal()
+        // {
+        //     decimal inputAsDecimal = Console.ReadLine();
+        //     return inputAsDecimal;
+        // }
+        // public DateTime AgentInputDateTime()
+        // {
+        //     DateTime inputAsDateTime = Console.ReadLine();
+        //     return inputAsDateTime.ToShortDateString();
+        // }
+        // public bool AgentInputBool()
+        // {
+        //     decimal inputAsBool = Console.ReadLine();
+        //     return inputAsBool;
         
+        public void PrintClaim(Claim claim)
+        {
+            Console.WriteLine($"{claim.ClaimID} {claim.TypeOfClaim} {claim.Description} ${claim.ClaimAmount} {claim.DateOfIncident.ToShortDateString()}  {claim.DateOfClaim.ToShortDateString()} {claim.IsValid}"); 
+        }
+
+        public bool ValidClaim(Claim claim)
+        {
+            long x = long.Parse(claim.DateOfClaim).ToString("yyyyMMddHHmmss");
+            long y = long.Parse(claim.DateOfIncident).ToString("yyyyMMddHHmmss");
+            int result = int.Parse(x - y);
+            if(x >= 30)
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
     }
 }
--- KomodoClaims/KomodoClaims.ConsoleApp/UserInterface.cs
+++ KomodoClaims/KomodoClaims.ConsoleApp/UserInterface.cs
@@ -8,6 +8,111 @@ namespace KomodoClaims.ConsoleApp
 {
     public class UserInterface
     {
-        
+        private bool isRunning = true;
+
+        ClaimsRepository _claimsRepo = new ClaimsRepository();
+        CustomConsole _customConsole = new CustomConsole();
+
+        public void Run()
+        {
+            _claimsRepo.SeedClaimsData();
+
+            while(isRunning)
+            {
+                _customConsole.PrintMainMenu();
+
+                string input = _customConsole.AgentInput();
+
+                MainMenuSelectionSwitchCase(input);
+            }
+        }
+
+        private void MainMenuSelectionSwitchCase(string input)
+        {
+            switch(input)
+            {
+                case "1":
+                    ViewAllClaims();
+                    break;
+                case "2":
+                    TakeCareOfNextClaim();
+                    break;
+                case "3":
+                    EnterNewClaim();
+                default:
+                    Console.WriteLine("Please try again by selecting one of the above options.");
+                    break;
+            }
+        }
+
+        private void ViewAllClaims()
+        {
+            List<Claim> allClaims = _claimsRepo.GetClaims();
+            _customConsole.PrintClaimsHeadings();
+            foreach(Claim claim in allClaims)
+            {
+                _customConsole.PrintClaim(claim);
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        public void TakeCareOfNextClaim()
+        {
+            Queue<Claim> allClaims = new Queue<Claim>(_claimsRepo.GetClaims());
+            foreach(Claim claim in allClaims)
+            {
+                _customConsole.PrintClaimsHeadings();
+                _customConsole.PrintClaim(claim);
+                Console.Write("Do you want to deal with this claim right now?(y/n): ");
+                string input = Console.ReadLine();
+                if(input.ToUpper() == "Y")
+                {
+                    Console.WriteLine("The claim will now be removed from the queue");
+                    allClaims.Dequeue();
+                }
+                else if(input.ToUpper() == "N")
+                {
+                    _customConsole.PrintMainMenu();
+                }
+                else
+                {
+                    Console.WriteLine("I'm sorry, we didn't understand. You are now being redirected to the main menu. Please try again.");
+                }
+            }
+        }
+
+        public void EnterNewClaim()
+        {
+            Console.WriteLine("Please answer the following prompts to add a new claim to the working list.");
+
+            Console.Write("\nEnter the claim ID: ");
+            string claimID = _customConsole.AgentInput();
+            
+            Console.Write("\nEnter the claim type: ");
+            string claimType = _customConsole.AgentInput();
+
+            Console.Write("\nEnter a claim description: ");
+            string claimDescription = _customConsole.AgentInput();
+
+            Console.Write("\nAmount of Damage: $ ");
+            string description = _customConsole.AgentInput();
+
+            Console.Write("\nDate Of Accident: ");
+            string dateOfAccident = _customConsole.AgentInput();
+
+            Console.Write("\nDate Of Claim: ");
+            string dateOfClaim = _customConsole.AgentInput();
+
+            if(_customConsole.ValidClaim() == true)
+            {
+                Console.Write("This is a valid claim");
+            }
+            else
+            {
+                Console.Write("This is not a valid claim");
+            }
+
+        }
     }
 }
